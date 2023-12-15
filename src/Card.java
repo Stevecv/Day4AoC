35a5acc8dfc89999f6e49f2228bff14da8af4c81
Jackson Perry <40204509@hruc.ac.uk>
2023-12-15 11:13:09 +0000

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Card {
+    public int cardNum;
+    public int[] winningNumbers;
+    public int[] numbers;
+    public int score;
+    public Card(String card) {
+        String cleanedCard = card.replace("Card ", "")
+                .replace(" | ", "|")
+                .replace(": ", "|");
+        String[] cardPieces = cleanedCard.split("\\|");
+
+        cardNum = Integer.parseInt(cardPieces[0].trim());
+
+        ArrayList<Integer> winningNums = new ArrayList<>();
+        for (String winningNum: cardPieces[1].split(" ")) {
+            if (!winningNum.isEmpty()) {
+                winningNums.add(Integer.valueOf(winningNum.trim()));
+            }
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (String num: cardPieces[2].split(" ")) {
+            if (!num.isEmpty()) {
+                nums.add(Integer.valueOf(num.trim()));
+            }
+        }
+        winningNumbers = winningNums.stream().mapToInt(i -> i).toArray();
+        numbers = nums.stream().mapToInt(i -> i).toArray();
+        calculateScore();
+    }
+
+    private void calculateScore() {
+        int totalledScore = 0;
+        int winningCardCount = 0;
+        for (int num: numbers) {
+            for (int winningNum: winningNumbers) {
+                if (num == winningNum) {
+                    winningCardCount++;
+                    break;
+                }
+            }
+        }
+        for (int i = cardNum; i < cardNum+winningCardCount; i++) {
+            Main.cards.add(Main.cards.get(i));
+        }
+        score = totalledScore;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static ArrayList<String> cards = new ArrayList<>(List.of(PuzzleInput.puzzleInput.split("\n")));
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            new Card(cards.get(i));
+        }
+        System.out.println(cards.size());
+    }
+}
